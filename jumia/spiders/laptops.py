8e57c16b99eed3387f6c6b5b9a7b7eafc090ee82
Job Kilonzo <jobkilonzo95@gmail.com>
2019-06-10 13:42:22 +0300

    First commit

---
+++ jumia/spiders/laptops.py
@@ -0,0 +1,15 @@
+import scrapy
+
+class LaptopsSpider(scrapy.Spider):
+    name="laptops"
+    start_urls = [
+        'https://www.jumia.co.ke/laptops/'
+    ]
+
+    def parse(self, response):
+        for laptops in response.xpath("//*[contains(@class, '-gallery')]"):
+            yield{
+                "brand": laptops.xpath(".//span[contains(@class, 'brand')]/text()").extract_first(),
+                "name": laptops.xpath(".//span[@class='name']/text()").extract_first(),
+                "price": laptops.xpath(".//span[@class='price-box ri']/span[contains(@class, 'price')][1]/span[@dir='ltr']/text()").extract_first()
+            }
